79da267d89c6d2a6458a0cc237e442de62d1a195
Sam <sam.fordham1@gmail.com>
2020-09-10 16:47:25 +0100

    Amended input component

--- src/components/input.js
+++ src/components/input.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Input = () => {
     return(
         <div>
-            <input value={name} onChange={e => setName(e.target.value)} placeholder="name" />
-            <input value={age} onChange={e => setAge(e.target.value)} placeholder="age" />
+            <input value={name} onChange={e => setName(e.target.value)} placeholder="Name" />
+            <input value={email} onChange={e => setEmail(e.target.value)} placeholder="Email address" />
         </div>
     )
 }
