359e7989642a6c266c6be18c8c9682254e95891f
Sam <sam.fordham1@gmail.com>
2020-10-07 21:47:27 +0100

    My work so far, need confirmation on features such as the add button. Style needs fixing too

--- src/components/data.js
+++ src/components/data.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import './data.css'
 
-
 const SaleItem = (props) => {
 
     return (
-        <div className="Box" onClick={() => {props.select(props.id)}} id={props.id}>
-                <img className="Picture"src={props.image}/>
+        <div className="Box" onClick={() => {props.select(props.id)}} id={props.id} 
+        color1={props.color1} image1={props.image1} 
+        color2={props.color2} image2={props.image2}
+        color3={props.color3} image3={props.image3}>
+                <img className="Picture" src = {props.image}/>
                 <p>{props.name}</p>
                 <p>{props.price}</p>
         </div>
     )
 }
 
-
 export default SaleItem
+
+
